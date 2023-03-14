793101eef2ece7da30f4e5087f5d5197528979d6
Kundai <kundai@mohara.co>
2023-03-14 14:08:59 +0200

    added new pages and components

---
+++ src/api/page/controllers/page.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ *  post controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::page.page", ({ strapi }) => ({
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const query = {
+      filters: { slug },
+      ...ctx.query,
+    };
+
+    const post = await strapi.entityService.findMany("api::page.page", query);
+
+    const sanitizedEntity = await this.sanitizeOutput(post);
+
+    return this.transformResponse(sanitizedEntity[0]);
+  },
+}));
