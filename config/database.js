90dd8261f02b5d9ea40d7552ea5e7f29f6acd16d
Seshan Pillay <seshan@mohara.co>
2023-03-16 12:22:29 +0200

    Update

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
-module.exports = ({ env }) => ({
+const path = require("path");
+
+module.exports = ({env}) => ({
   connection: {
-    client: 'sqlite',
+    client: "sqlite",
     connection: {
       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
