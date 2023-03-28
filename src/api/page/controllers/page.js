daac052f53b299b265697b0217b3548521b8b441
Dominic Walsh <djcwalsh94@gmail.com>
2023-03-28 11:53:34 +0200

    updated routes

--- src/api/page/controllers/page.js
+++ src/api/page/controllers/page.js
@@ -8,11 +8,26 @@ const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::page.page", ({ strapi }) => ({
   //Change to findById
-  async findOneBySlug(ctx) {
+  async findOne(ctx) {
     const { slug } = ctx.params;
 
     const query = {
       filters: { slug },
+      ...ctx.query,
+    };
+
+    const post = await strapi.entityService.findMany("api::page.page", query);
+
+    const sanitizedEntity = await this.sanitizeOutput(post);
+
+    return this.transformResponse(sanitizedEntity[0]);
+  },
+
+  async findOnebyId(ctx) {
+    const { id } = ctx.params;
+
+    const query = {
+      filters: { id },
       ...ctx.query,
     };
 
